a9acf4313bc542ec1820e34f84b9fbbb1983dcfc
Saurabh Bachawe <sb4514.work@gmail.com>
2021-09-13 22:55:40 +0530

    ft: list all default tasks

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
 use std::env;
 
+struct TodoItem {
+    name: String,
+    completed: char
+}
+
 fn main() {
     let arg: Vec<String> = env::args().collect();
     let command = &arg[1];
-    println!("{:#?}", command);
+
+    // example todo
+    let todo_item = TodoItem {
+        name: "Go thru the solana docs".to_string(),
+        completed: ' '
+    };
+
+    let todo_list = vec![todo_item];
+    
+    match command.as_str() {
+        "get" => {
+            for item in todo_list {
+                println!("[{}] - {}", item.completed, item.name);
+            }
+        },
+        _ => return (),
+    }
 }
+ 
