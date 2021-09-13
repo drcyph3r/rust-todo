b5fc8bb7c587754aa7eb9cc5d675323f897be9db
Saurabh Bachawe <sb4514.work@gmail.com>
2021-09-13 23:26:37 +0530

    ft: `TodoList` implementation

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,10 @@ struct TodoItem {
     completed: char
 }
 
+struct TodoList {
+    list: Vec<TodoItem>
+}
+
 impl TodoItem {
     fn create(name: String) -> TodoItem {
        return TodoItem {
@@ -14,20 +18,42 @@ impl TodoItem {
     }
 }
 
+impl TodoList {
+    // create a new list
+    fn new() -> TodoList {
+        return TodoList{list: Vec::new()};
+    }
+
+    // add todo item to list
+    fn add(&mut self, name: String) {
+        let item = TodoItem::create(name);
+        self.list.push(item);
+    }
+
+    // display todo list
+    fn display(&self) {
+        for item in &self.list {
+            println!("[{}] - {}", item.completed, item.name);
+        }
+    }
+}
+
 fn main() {
+    // collect the arguments from command line
     let arg: Vec<String> = env::args().collect();
+    // extract the command from the arguments
     let command = &arg[1];
 
-    // example todo
-    let todo_item_1 = TodoItem::create("Go thru the solana docs.".to_string());
-
-    let todo_list = vec![todo_item_1];
+    // initialize the empty todo list
+    let todo_list = TodoList::new();
     
+    // do todo operations according to command received
     match command.as_str() {
         "get" => {
-            for item in todo_list {
-                println!("[{}] - {}", item.completed, item.name);
-            }
+            todo_list.display()
+        },
+        "add" => {
+
         },
         _ => return (),
     }
